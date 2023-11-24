f19d23486c3ce79dedea7aeb8184683f4f6b9ce5
ShawnZhuPro <shawnz05827@gmail.com>
2023-11-23 19:31:59 -0800

    Implement search results

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
+  API_KEY: "5c4710e79ebd24278e9ed66fc6c66084",
+  API_URL: "https://api.themoviedb.org/3/",
 };
 
 // Displays 20 most popular movies
@@ -240,6 +248,71 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== "" && global.search.term !== null) {
+    // Make request and display results
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No results found");
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector("#search-term").value = "";
+  } else {
+    showAlert("Please enter a search term");
+  }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${
+                global.search.type === "movie" ? result.title : result.name
+              }"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${global.search.type === "movie" ? result.title : result.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${
+              global.search.type === "movie" ? result.title : result.name
+            }</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${
+                global.search.type === "movie"
+                  ? result.release_date
+                  : result.first_air_date
+              }</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector("#search-results").appendChild(div);
+  });
+}
+
 // Display movies in a slider
 async function displaySlider() {
   // Restructure results
@@ -304,13 +377,25 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = "5c4710e79ebd24278e9ed66fc6c66084";
-  const API_URL = "https://api.themoviedb.org/3/";
-
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&languag=en-US`
+    `${global.API_URL}${endpoint}?api_key=${global.API_KEY}&languag=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+// Make request to search
+async function searchAPIData() {
+  showSpinner();
+
+  const response = await fetch(
+    `${global.API_URL}search/${global.search.type}?api_key=${global.API_KEY}&languag=en-US&query=${global.search.term}`
   );
 
   const data = await response.json();
@@ -338,17 +423,10 @@ function highlightActiveLink() {
   });
 }
 
-function addCommasToNumber(number) {
-  // Regular expression
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
 // Function to fetch and display movie trailer
 async function fetchMovieTrailer(movieId) {
-  const API_KEY = "5c4710e79ebd24278e9ed66fc6c66084"; // Replace with your TMDb API key
-
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${global.API_KEY}&language=en-US`
   );
   const data = await response.json();
 
@@ -361,6 +439,21 @@ async function fetchMovieTrailer(movieId) {
         <iframe width="560" height="315" src="https://www.youtube.com/embed/${trailerKey}" frameborder="0" allowfullscreen></iframe>
       `;
   }
+}
+
+// Show Alert
+function showAlert(message, className = "error") {
+  const alertEl = document.createElement("div");
+  alertEl.classList.add("alert", className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector("#alert").appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
+function addCommasToNumber(number) {
+  // Regular expression
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 // Init App
@@ -381,6 +474,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
+      search();
       break;
   }
 
