be1b10af5252554f1b2a7e7e5fe4f2934bb0a03c
ShawnZhuPro <shawnz05827@gmail.com>
2023-11-23 17:02:13 -0800

    Add a movie details page

--- js/script.js
+++ js/script.js
@@ -45,31 +45,102 @@ async function displayPopularShows() {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
-          <a href="tv-details.html?id=${show.id}">
-            ${
-              show.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-              class="card-img-top"
-              alt="${show.name}"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
+        <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
             class="card-img-top"
             alt="${show.name}"
           />`
-            }
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">${show.name}</h5>
-            <p class="card-text">
-              <small class="text-muted">Air Date: ${show.first_air_date}</small>
-            </p>
-          </div>
-        `;
+              : `<img
+          src="../images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+      `;
 
     document.querySelector("#popular-shows").appendChild(div);
   });
+}
+
+// Display movie details
+async function displayMovieDetails() {
+  // Gives us the movie ID through parsing the URL
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+      <div class="details-top">
+      <div>
+      ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+      }
+      </div>
+      <div>
+        <h2>${movie.title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+          ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+        </ul>
+        <a href="${
+          movie.homepage
+        }" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+          movie.budget
+        )}</li>
+        <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+          movie.revenue
+        )}</li>
+        <li><span class="text-secondary">Runtime:</span> ${
+          movie.runtime
+        } minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+        ${movie.production_companies
+          .map((company) => `<span>${company.name}</span>`)
+          .join(", ")}
+      </div>
+    </div>
+      `;
+
+  document.querySelector("#movie-details").appendChild(div);
 }
 
 // Fetch data from TMDB API
@@ -108,6 +179,11 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  // Regular expression
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -119,6 +195,7 @@ function init() {
       displayPopularShows();
       break;
     case "/movie-details.html":
+      displayMovieDetails();
       break;
     case "tv-details.html":
       break;
@@ -126,7 +203,7 @@ function init() {
       break;
   }
 
-  highlightActiveLink;
+  highlightActiveLink();
 }
 
 document.addEventListener("DOMContentLoaded", init);
